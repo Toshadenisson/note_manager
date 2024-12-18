cad89a7af7c087a0e0be71f4afbf3b5cc76e46de
Toshadenisson <toshadenisson@yandex.ru>
2024-12-18 21:38:08 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username='"Имя пользователя:"'
+title='"Заголовок заметки:"'
+content='"Описание заметки:"'
+status='"Статус заметки:"'
+created_date='"Дата создания (дд.мм.гг.)"'
+issue_date='"Дата окончания (дд.мм.гг.)"'
+print(username,'username')
+print(title,'title')
+print(content,'content')
+print(status,'status')
+print(created_date,'created_date')
+print (issue_date,'issue_date')
