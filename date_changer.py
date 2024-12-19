dffd701af827c5dba6c1f7d6b0f33c817c039776
Toshadenisson <toshadenisson@yandex.ru>
2024-12-19 22:34:46 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,24 @@
+username='"Имя пользователя:"'
+title='"Заголовок заметки:"'
+content='"Описание заметки:"'
+status='"Статус заметки:"'
+created_date='"Дата создания (дд.мм.гг.)"'
+issue_date='"Дата окончания (дд.мм.гг.)"'
+print(username,'username')
+print(title,'title')
+print(content,'content')
+print(status,'status')
+print(created_date,'created_date')
+print (issue_date,'issue_date')
+username='"Ваше Имя:"'
+title='"  заголовок:"'
+content='"Содержание:"'
+status='"Статус:"'
+created_date='"начало (дд.мм.гг.)"'
+issue_date='"конец (дд.мм.гг.)"'
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print (issue_date)
