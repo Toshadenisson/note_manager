5d1ed1ce850006287482d382133772f4fd6480b4
Toshadenisson <toshadenisson@yandex.ru>
2024-12-23 22:36:38 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,24 +1,6 @@
-username='"Имя пользователя:"'
-title='"Заголовок заметки:"'
-content='"Описание заметки:"'
-status='"Статус заметки:"'
-created_date='"Дата создания (дд.мм.гг.)"'
-issue_date='"Дата окончания (дд.мм.гг.)"'
-print(username,'username')
-print(title,'title')
-print(content,'content')
-print(status,'status')
-print(created_date,'created_date')
-print (issue_date,'issue_date')
-username='"Ваше Имя:"'
-title='"  заголовок:"'
-content='"Содержание:"'
-status='"Статус:"'
-created_date='"начало (дд.мм.гг.)"'
-issue_date='"конец (дд.мм.гг.)"'
-print(username)
-print(title)
-print(content)
-print(status)
-print(created_date)
-print (issue_date)
+created_date='10.11.24'
+issue_date='10.12.24'
+temp_created_date=created_date[0:5]
+temp_issue_date=issue_date [0:5]
+print(temp_created_date)
+print(temp_issue_date)
