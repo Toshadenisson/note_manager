17b11aaf69c2bbd4461c7924f2c75a2957ca2127
Toshadenisson <toshadenisson@yandex.ru>
2024-12-24 23:58:06 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,9 +1,13 @@
-
-username=input('Введите Ваше Имя ')
+username=input('Введите Ваше Имя: ')
 content=input('Введите описание заметки: ')
 status=input('Введите статус заметки: ')
-created_date=input('Введите дату создания (дд.мм.гг.) ')
-issue_date=input('Введите дату окончания (дд.мм.гг.) ')
-title_list=[input('Введите заголовок 1 '), input('Введите заголовок 2 '), input('Введите заголовок 3 ')]
-note=[username,content,status,created_date,issue_date,title_list]
+created_date=input('Введите дату создания (дд.мм.гг.): ')
+issue_date=input('Введите дату окончания (дд.мм.гг.): ')
+title=[]
+for i in range (3):
+    x=input('Введите заголовок: ')
+    title.append(x)
+values=[username,content,status,created_date,issue_date,title]
+key_note=["Имя пользователя ","Описание заметки ","Статус заметки ","Дата создания ","Дата окончания ", 'titles']
+note=dict(zip(key_note,values))
 print(note)
