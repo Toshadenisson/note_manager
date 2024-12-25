ab7fd500468daadc55a06e4e61d757e40c022a01
Toshadenisson <toshadenisson@yandex.ru>
2024-12-25 20:01:05 +0300

    Update final.py

--- final.py
+++ final.py
@@ -10,4 +10,9 @@ for i in range (3):
 values=[username,content,status,created_date,issue_date,title]
 key_note=["Имя пользователя ","Описание заметки ","Статус заметки ","Дата создания ","Дата окончания ", 'titles']
 note=dict(zip(key_note,values))
-print(note)
+print('Имя пользователя - ',note["Имя пользователя "])
+print('Описание заметки - ',note["Описание заметки "])
+print('Статус заметки - ',note["Статус заметки "])
+print('Дата создания - ',note["Дата создания "])
+print('Дата окончания - ',note ["Дата окончания "])
+print('Список заголовков: ',note['titles'])
