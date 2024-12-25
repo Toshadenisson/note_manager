9f272344bc2ec8e34e299a69e852caf42143b02a
Toshadenisson <toshadenisson@yandex.ru>
2024-12-25 20:00:16 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,6 +1,6 @@
 created_date='10.11.24'
 issue_date='10.12.24'
-temp_created_date=created_date[0:5]
-temp_issue_date=issue_date [0:5]
+temp_created_date=created_date[:5]
+temp_issue_date=issue_date [:5]
 print(temp_created_date)
 print(temp_issue_date)
